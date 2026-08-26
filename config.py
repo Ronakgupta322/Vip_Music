f296f3b4ff672c8b3eeb08e987ccf7388e495315
Ronak Gupta <ronakgupta640@gmail.com>
2026-08-26 13:44:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/denki307/DENKIMUSICROBOT",
+    "https://github.com/Ronakgupta322/Vip_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
